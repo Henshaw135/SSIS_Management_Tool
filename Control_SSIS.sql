b1dd81e81fda6e6ced58d670f2bac35fb1512f36
jhenshaw135 <55221682+jhenshaw135@users.noreply.github.com>
2019-09-11 20:45:01 -0700

    Update Control_SSIS.sql
    Auto populated ID_Number for SP's temp table creation

--- Control_SSIS.sql
+++ Control_SSIS.sql
@@ -1,6 +1,6 @@
 CREATE TABLE [SSIS].[Control_SSIS]
 (
-	ID_Number			INT			NOT NULL,
+	ID_Number			INT			NOT NULL	IDENTITY(1,1),
 	SSIS_Package_Name		VARCHAR(40)		NOT NULL,
 	Report_Group			VARCHAR(30)		NOT NULL,
 	Frequency			VARCHAR(15)		NOT NULL,
