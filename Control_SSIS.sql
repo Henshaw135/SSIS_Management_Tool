6990da506ef454bc38d74e58b34d3de84d9c7615
jhenshaw135 <55221682+jhenshaw135@users.noreply.github.com>
2019-09-11 20:18:43 -0700

    Create Control_SSIS.sql
    Control Table for SSIS Packages.

---
+++ Control_SSIS.sql
@@ -0,0 +1,9 @@
+CREATE TABLE [SSIS].[Control_SSIS]
+(
+	ID_Number			INT			NOT NULL,
+	SSIS_Package_Name		VARCHAR(40)		NOT NULL,
+	Report_Group			VARCHAR(30)		NOT NULL,
+	Frequency			VARCHAR(15)		NOT NULL,
+	SSIS_Dir			VARCHAR(256)		NOT NULL,
+	IsEnabledYN			VARCHAR(3)		NOT NULL
+)
